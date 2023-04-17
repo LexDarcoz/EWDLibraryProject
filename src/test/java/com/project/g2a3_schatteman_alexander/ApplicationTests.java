827443868879805aebb35c97620c90b7bd7f9f40
LexDarcoz <Alexander.schatteman@gmail.com>
2023-04-17 18:49:29 +0200

    Initial Commit For project EWD

---
+++ src/test/java/com/project/g2a3_schatteman_alexander/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.g2a3_schatteman_alexander;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
