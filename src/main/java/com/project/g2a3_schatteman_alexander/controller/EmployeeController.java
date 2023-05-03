0bb51d41d6313efb67770f9f7cee874e2b4afd9f
LexDarcoz <Alexander.schatteman@gmail.com>
2023-05-03 13:24:42 +0200

    V0.1.0

--- src/main/java/com/project/g2a3_schatteman_alexander/controller/EmployeeController.java
+++ src/main/java/com/project/g2a3_schatteman_alexander/controller/EmployeeController.java
@@ -1,23 +1,38 @@
 package com.project.g2a3_schatteman_alexander.controller;
 
+
 import com.project.g2a3_schatteman_alexander.entities.Employee;
 import com.project.g2a3_schatteman_alexander.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
+
 @Controller
 public class EmployeeController {
-
+    @Autowired
     private EmployeeRepository eRepo;
 
-    @GetMapping("/showEmployees")
+
+    @GetMapping({"/showEmployees", "/", "/list"})
     public ModelAndView showsEmployees() {
         ModelAndView mav = new ModelAndView("list-employees");
         List<Employee> list = eRepo.findAll();
         mav.addObject("employees", list);
         return mav;
+
     }
+
+    @GetMapping("/addEmployeeForm")
+    public ModelAndView addEmployeeForm() {
+        ModelAndView mav = new ModelAndView("add-employee-form");
+        Employee newEmployee = new Employee();
+        mav.addObject("employee", newEmployee);
+        return mav;
+    }
+
+
 }
--- src/test/java/com/project/g2a3_schatteman_alexander/ApplicationTests.java
+++ src/test/java/com/project/g2a3_schatteman_alexander/ApplicationTests.java
@@ -3,6 +3,7 @@ package com.project.g2a3_schatteman_alexander;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class ApplicationTests {
 
