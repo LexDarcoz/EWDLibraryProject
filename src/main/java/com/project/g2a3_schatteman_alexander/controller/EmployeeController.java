3fce208ac05985580a190d96f46b62af8a0284f7
LexDarcoz <Alexander.schatteman@gmail.com>
2023-05-07 23:22:15 +0200

    V0.1.0

--- src/main/java/com/project/g2a3_schatteman_alexander/controller/EmployeeController.java
+++ src/main/java/com/project/g2a3_schatteman_alexander/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -19,9 +20,15 @@ public class EmployeeController {
     private EmployeeRepository eRepo;
 
 
-    @GetMapping({"/showEmployees", "/", "/list"})
-    public ModelAndView showsEmployees() {
-        ModelAndView mav = new ModelAndView("list-employees");
+    @GetMapping({"/home", "/"})
+    public ModelAndView showLandingPage() {
+        ModelAndView mav = new ModelAndView("landingPage");
+        return mav;
+    }
+
+    @GetMapping({"/showEmployees", "/list"})
+    public ModelAndView showEmployees() {
+        ModelAndView mav = new ModelAndView("employees/list-employees");
         List<Employee> list = eRepo.findAll();
         mav.addObject("employees", list);
         return mav;
@@ -30,10 +37,25 @@ public class EmployeeController {
 
     @GetMapping("/addEmployeeForm")
     public ModelAndView addEmployeeForm() {
-        ModelAndView mav = new ModelAndView("add-employee-form");
+        ModelAndView mav = new ModelAndView("employees/add-employee-form");
         Employee newEmployee = new Employee();
         mav.addObject("employee", newEmployee);
         return mav;
+    }
+
+    @GetMapping("/showUpdateForm")
+    public ModelAndView showUpdateForm(@RequestParam Long employeeId) {
+        ModelAndView mav = new ModelAndView("employees/add-employee-form");
+        System.out.println("aaaaaaaaaaaaaaaaaaaa");
+        Employee employee = eRepo.getReferenceById(employeeId);
+        mav.addObject("employee", employee);
+        return mav;
+    }
+
+    @GetMapping("/deleteEmployee")
+    public String deleteEmployee(@RequestParam long employeeId) {
+        eRepo.deleteById(employeeId);
+        return "redirect:/list";
     }
 
     @PostMapping("/saveEmployee")
